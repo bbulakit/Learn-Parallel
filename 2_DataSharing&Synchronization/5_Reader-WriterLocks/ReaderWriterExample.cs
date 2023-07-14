069ad8384bebc6dfda71ebc50ce247cd21dd2c63
bbulakit <b.bulakit@hotmail.com>
2023-07-14 22:57:44 +0700

    Init ReaderWriterExample

---
+++ 2_DataSharing&Synchronization/5_Reader-WriterLocks/ReaderWriterExample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_Parallel._2_DataSharing_Synchronization._5_Reader_WriterLocks
+{
+    internal class ReaderWriterExample
+    {
+    }
+}
