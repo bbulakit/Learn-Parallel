1a0d63f8e7d114176e3075310e692ff9cc3deca8
bbulakit <b.bulakit@hotmail.com>
2023-07-14 14:26:09 +0700

    Init

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal static class Program
+    {
+        public static void Write(char c)
+        {
+            int i = 1_000;
+            while(i-->0)
+            {
+                Console.Write(c);
+            }
+        }
+        public static void Main()
+        {
+            Task.Factory.StartNew(() => Write('*')); //Thread1
+
+            var t = new Task(() => Write('?')); //Thread2
+            t.Start();
+
+            Write('-'); //Main Thread
+
+            Console.WriteLine("Main program done.");
+            Console.ReadKey();
+        }
+    }
+}
