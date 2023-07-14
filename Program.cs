42a6e01a5f07a1f7e74a9b43aa11128668f9a35c
bbulakit <b.bulakit@hotmail.com>
2023-07-14 18:17:43 +0700

    Added CriticalSections for bank accounting example

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Learn_Parallel._1_TaskProgramming._2_CancellingTasks;
 using Learn_Parallel._1_TaskProgramming._3_WaitingForTimeToPass;
 using Learn_Parallel._1_TaskProgramming._4_WaitingForTasks;
 using Learn_Parallel._1_TaskProgramming._5_ExceptionHandling;
+using Learn_Parallel._2_DataSharing_Synchronization._1_CriticalSections;
 using System;
 using System.Threading.Tasks;
 
@@ -12,12 +13,18 @@ namespace Demo
     {
         public static void Main()
         {
+            #region "1_TaskProgramming"            
             //TaskCreatingAndStarting.Start();
             //CancellingTasks.Start();
             //CancellingTasks.Start2();
             //WaitingForTimeToPass.Start();
             //WaitingForTasks.Start();
-            ExceptionHandling.Start();
+            //ExceptionHandling.Start();
+            #endregion
+
+            #region "2_DataSharing&Synchronization"
+            CriticalSections.Start();
+            #endregion
         }
     }
 }
