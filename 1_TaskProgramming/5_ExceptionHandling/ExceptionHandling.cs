9edc85132b190fa5e07ff7eec0e62c18ea509029
bbulakit <b.bulakit@hotmail.com>
2023-07-14 17:33:04 +0700

    Handle multiple exceptions at once

--- 1_TaskProgramming/5_ExceptionHandling/ExceptionHandling.cs
+++ 1_TaskProgramming/5_ExceptionHandling/ExceptionHandling.cs
@@ -10,27 +10,46 @@ namespace Learn_Parallel._1_TaskProgramming._5_ExceptionHandling
     {
         public static void Start()
         {
-            var t = Task.Factory.StartNew( () =>{
-                throw new InvalidOperationException("Can't do this!") { Source = "t2" };
-            });
-
-            var t2 = Task.Factory.StartNew(() => {
-                throw new AccessViolationException("Can't access this!") { Source = "t2" };
-            });
-
             try
-            {                
-                Task.WaitAll(t, t2);
+            {
+                Test();
             }
             catch (AggregateException ae)
             {
                 foreach (var e in ae.InnerExceptions)
-                    Console.Write($"Exception {e.GetType()} from {e.Source}");
-            }            
-
+                    Console.WriteLine($"Handled elsewhere: {e.GetType()} from {e.Source}");
+            }
 
             Console.WriteLine("ExceptionHadling Program done.");
             Console.ReadKey();
+        }
+
+        private static void Test()
+        {
+            var t = Task.Factory.StartNew(() =>
+            {
+                throw new InvalidOperationException("Can't do this!") { Source = "t2" };
+            });
+
+            var t2 = Task.Factory.StartNew(() =>
+            {
+                throw new AccessViolationException("Can't access this!") { Source = "t2" };
+            });
+
+            try
+            {
+                Task.WaitAll(t, t2);
+            }
+            catch (AggregateException ae) //Represents one or more errors that occur during application execution.
+            {
+                ae.Handle(e =>
+                {
+                    if (e is InvalidOperationException) {
+                        Console.WriteLine("Invalid op!");
+                        return true;
+                    }return false;
+                });
+            }
         }
     }
 }
